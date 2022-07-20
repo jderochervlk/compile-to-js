158659b6b30ca6611bf8e411679fb6391eaf378b
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2022-07-20 10:06:02 -0400

    chore: add prettier and format

--- plain-js/public/index.js
+++ plain-js/public/index.js
@@ -1,11 +1,11 @@
-(function(){
-    console.info('Project is working as expected...')
+(function () {
+  console.info("Project is working as expected...");
 
-    const targetEl = document.getElementById("content")
+  const targetEl = document.getElementById("content");
 
-    fetch("https://baconipsum.com/api/?type=meat-and-filler")
-        .then(res => res.json())
-        .then(strings => strings.map(str => `<p>${str}</p>`).join('<br />'))
-        .then(content => targetEl.innerHTML = content)
-        .catch(console.error)
-})()
+  fetch("https://baconipsum.com/api/?type=meat-and-filler")
+    .then((res) => res.json())
+    .then((strings) => strings.map((str) => `<p>${str}</p>`).join("<br />"))
+    .then((content) => (targetEl.innerHTML = content))
+    .catch(console.error);
+})();
--- server/app.js
+++ server/app.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const morgan = require('morgan')
+const morgan = require("morgan");
 
-morgan('dev')
+morgan("dev");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) =>
-    res.send('Hello!'))
+app.get("/", (req, res) => res.send("Hello!"));
 
-app.use('/plain-js/',
-    express.static(path.join(__dirname, '../plain-js/public/')))
+app.use(
+  "/plain-js/",
+  express.static(path.join(__dirname, "../plain-js/public/"))
+);
 
-app.use('/react-fp-ts/',
-    express.static(path.join(__dirname, '../react-fp-ts/dist/')))
+app.use(
+  "/react-fp-ts/",
+  express.static(path.join(__dirname, "../react-fp-ts/dist/"))
+);
 
 app.use((req, res, next) => {
-    res.status(404).send("Sorry, can't find that!")
-})
+  res.status(404).send("Sorry, can't find that!");
+});
 
-
-module.exports = app
+module.exports = app;
