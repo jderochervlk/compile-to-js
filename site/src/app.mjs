c75ec7e3125d430e6b65c721d1bb331b862b6e23
Josh Vlk <josh@vlkpack.com>
2023-08-08 09:49:52 -0400

    compile rescript

--- site/src/app.mjs
+++ site/src/app.mjs
@@ -61,7 +61,7 @@ function App(props) {
           {
             className:
               'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
-            href: '/purescript',
+            href: '/plain-js',
           },
           text['plain-js']
         )
